27d4043424b254d02ca7f78f0e778d7d302c436b
Noransaber <noransaber685@gmail.com>
2023-07-21 21:36:29 +0300

    Tase 2

---
+++ Task1/sort.h
@@ -0,0 +1,30 @@
+#ifndef SORT_H
+#define SORT_H
+
+/** LIBRARU FILES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**DATA STRUCTURE FOR DOUBLY LINKED LIST*/
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/**FUNCTION PROTOTYPES*/
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void linklist_swapping(listint_t *sec, listint_t *first, listint_t **h);
+void insertion_sort_list(listint_t **list);
+#endif
