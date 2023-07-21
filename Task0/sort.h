cf8bf39884f4ae0e5d29c1841ab9a5868005765c
Ayowale <haydexwisdom@gmail.com>
2023-07-21 12:32:00 +0000

    First commit

---
+++ Task0/sort.h
@@ -0,0 +1,20 @@
+#ifndef SORT_H
+#define SORT_H
+
+
+/**DATA STRUCTURE FOR DOUBLY LINKED LIST*/
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#endif
