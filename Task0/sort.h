68d813529caed3eda0c40abec8141640d1955fbc
Ayowale <haydexwisdom@gmail.com>
2023-07-21 13:52:08 +0000

    Test passed

--- Task0/sort.h
+++ Task0/sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+/** LIBRARU FILES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**DATA STRUCTURE FOR DOUBLY LINKED LIST*/
 /**
@@ -16,5 +20,10 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/**FUNCTION PROTOTYPES*/
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif
